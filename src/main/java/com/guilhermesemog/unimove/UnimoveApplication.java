77e8f2fa65a643b980447ae342a91c686f9c8ea6
dionae1 <zguixmz@gmail.com>
2026-06-12 19:12:12 -0300

    feat: enable pages to be wrapped via dto

--- src/main/java/com/guilhermesemog/unimove/UnimoveApplication.java
+++ src/main/java/com/guilhermesemog/unimove/UnimoveApplication.java
@@ -2,8 +2,10 @@ package com.guilhermesemog.unimove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class UnimoveApplication {
 
     public static void main(String[] args) {
