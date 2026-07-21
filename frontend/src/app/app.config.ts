162b7b9220af41816654234be166e48751f8110a
guilhermesemog <zguixmz@gmail.com>
2026-07-21 15:03:10 -0300

    feat: add response interceptor and update auth interceptor for enhanced error handling

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,14 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './core/auth/auth.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { responseInterceptor } from './core/interceptors/response-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([authInterceptor])
+      withInterceptors([authInterceptor, responseInterceptor])
     ),
   ]
 };
