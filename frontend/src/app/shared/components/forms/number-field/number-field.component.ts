386da6d8b18fb5499b2f7f5a118c4c1b11d7c203
guilhermesemog <zguixmz@gmail.com>
2026-07-23 19:53:04 -0300

    feat: add numberField component

---
+++ frontend/src/app/shared/components/forms/number-field/number-field.component.ts
@@ -0,0 +1,24 @@
+import { Component, input, model } from '@angular/core';
+import { FormValueControl, WithOptionalFieldTree, ValidationError } from '@angular/forms/signals';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-number-field',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './number-field.component.html',
+})
+export class NumberFieldComponent implements FormValueControl<number> {
+  readonly value = model<number>(0);
+
+  readonly errors = input<readonly WithOptionalFieldTree<ValidationError>[]>([]);
+  readonly touched = model<boolean>(false);
+  readonly disabled = input<boolean>(false);
+  readonly required = input<boolean>(false);
+
+  readonly label = input.required<string>();
+  readonly placeholder = input<string>('');
+  readonly type = input<string>('number');
+  readonly id = input<string>();
+  readonly min = input<number>();
+}
