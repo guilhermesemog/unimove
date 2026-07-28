c77188051cfa8b1653ad66a943535e3da44b5887
guilhermesemog <zguixmz@gmail.com>
2026-07-28 16:15:55 -0300

    feat: add header component with title and description inputs

---
+++ frontend/src/app/shared/components/list-header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+  title = input.required<string>();
+  description = input<string>('');
+}
