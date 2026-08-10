8e6818cb22a1b442b78752525edf312623a176a3
guilhermesemog <zguixmz@gmail.com>
2026-08-10 15:54:42 -0300

    feat: enhance login page UI and form validation with improved structure and styling

--- frontend/src/app/features/auth/login/login.page.ts
+++ frontend/src/app/features/auth/login/login.page.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { form, FormField, required } from '@angular/forms/signals';
 
 import { AuthService } from '../../../core/auth/auth.service';
+import { TextFieldComponent } from '../../../shared/components/forms/text-field/text-field.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [FormField],
+  imports: [FormField, TextFieldComponent, MatIconModule],
   templateUrl: './login.html',
 })
 export class LoginPage {
@@ -18,17 +20,15 @@ export class LoginPage {
   });
 
   loginForm = form(this.loginModel, (schema) => {
-    required(schema.cpf);
-    required(schema.password);
+    required(schema.cpf, { message: 'CPF é obrigatório' });
+    required(schema.password, { message: 'Senha é obrigatória' });
   });
 
   async onSubmit() {
-
     if (!this.loginForm().valid()) {
       return;
     }
 
     await this.authService.login(this.loginModel().cpf, this.loginModel().password);
   }
-
 }
